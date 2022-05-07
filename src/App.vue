deb32a6d450a4a2bcdc0042a74d816444ae8696b
coderyxy <yu.life@qq.com>
2022-05-07 10:43:58 +0800

    style(login): coderyxy login style

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-
-
-
-
-
-
-
-
-
-
-  }
+  components: {}
 })
 </script>
 
